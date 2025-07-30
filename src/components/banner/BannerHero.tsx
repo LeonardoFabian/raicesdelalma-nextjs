56141bd0b35bc3a5f8765b0408d2fc3bfd3320bd
Leonardo Fabian <ramonlfabian@gmail.com>
2025-07-30 19:23:21 -0400

    Mobile menu

--- src/components/banner/BannerHero.tsx
+++ src/components/banner/BannerHero.tsx
@@ -15,7 +15,7 @@ export const BannerHero = ({ title, subtitle, cta }: Props ) => {
         <div id="banner-hero default-carousel" className="banner-hero relative bg-blue-pastel text-text-primary font-body rounded-lg h-screen w-full" data-carousel="slide">
             <Image className="w-full h-full object-cover relative" src={ banner } alt="hero" width={ 1920 } height={ 1080 } priority={false} />
             <div className="absolute top-0 left-0 right-0 z-10 h-full max-w-5xl mx-auto p-4 lg:p-24  overflow-hidden rounded-lg">
-                <div className="flex flex-col items-center justify-start pt-12 gap-4 h-full text-center" data-carousel-item>
+                <div className="flex flex-col items-center justify-start pt-32 md:pt-12 gap-4 h-full text-center" data-carousel-item>
                     {/* <Image className="p-4 w-full h-40 rounded-t-lg object-contain" src={ logo } alt="hero" width={ 250 } height={ 150 } priority={false} /> */}
                     <H1>{ title }</H1>
                     <h2 className="text-text-secondary text-md md:text-2xl">{ subtitle }</h2>
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,6 @@
-import { MdOutlineFavoriteBorder, MdOutlineShoppingCart } from "react-icons/md"
+'use client'
+
+import { MdOutlineFavoriteBorder, MdOutlineShoppingCart, MdMenu, MdOutlineClose } from "react-icons/md"
 import { ActiveLink, ActiveLinkProps } from "../active-link/ActiveLink"
 import { FavoritesCounterBadgeWidget } from "../widgets/FavoritesCounterBadge"
 import { CartCounterBadgeWidget } from "../widgets/CartCounterBadge"
@@ -6,6 +8,7 @@ import Link from "next/link"
 import Image from "next/image"
 import Logo from "@/public/logo.svg";
 import { HomeIcon } from "@primer/octicons-react"
+import { useState } from "react"
 
 
 const navItems: ActiveLinkProps[] = [
@@ -39,36 +42,66 @@ const accountLinks: ActiveLinkProps[] = [
 ]
 
 export const Header = () => {
+
+    const [open, setOpen] = useState(false);
+
+
+    const handleClick = () => {
+        setOpen(!open);
+    }
+
     return (
-        <nav className="absolute w-full top-0 left-0 right-0 z-50 grid grid-cols-3 bg-transparent text-text-secondary font-heading py-2 px-4">          
+        <>
+            <nav className="absolute w-full top-0 left-0 right-0 z-50 grid md:grid-cols-3 bg-transparent text-text-secondary font-heading py-2 px-4">          
 
-            <ul className="hidden md:flex items-center space-x-6">
-                {navItems.map(( navItem ) => (
-                    <ActiveLink key={navItem.path} {...navItem} />
-                ))}    
-            </ul>  
+                <ul className="hidden md:flex items-center space-x-6">
+                    {navItems.map(( navItem ) => (
+                        <ActiveLink key={navItem.path} {...navItem} />
+                    ))}    
+                </ul>  
 
-             <Link href="/" className="flex items-center justify-center">
-                {Image ? (
-                    <Image src={Logo} className="w-24 h-24" alt="Logo" height={54} />
-                ) : (
-                    <>
-                        <HomeIcon />
-                        Home
-                    </>
-                )}
-            </Link>
-
-            <ul className="hidden md:flex items-center justify-end space-x-6">
-                {accountLinks.map(( accountLink ) => (
-                    <ActiveLink key={accountLink.path} {...accountLink} />
-                ))}    
-
-                <Link href="/login" className="flex items-center py-2 px-4 transition-all bg-primary hover:bg-accent text-white font-heading font-semibold rounded-lg">
-                    Log In
+                <Link href="/" className="flex items-center justify-center">
+                    {Image ? (
+                        <Image src={Logo} className="w-24 h-24" alt="Logo" height={54} />
+                    ) : (
+                        <>
+                            <HomeIcon />
+                            Home
+                        </>
+                    )}
                 </Link>
-            </ul>              
 
-        </nav>
+                <ul className="hidden md:flex items-center justify-end space-x-6">
+                    {accountLinks.map(( accountLink ) => (
+                        <ActiveLink key={accountLink.path} {...accountLink} />
+                    ))}    
+
+                    <Link href="/login" className="flex items-center py-2 px-4 transition-all bg-primary hover:bg-accent text-white font-heading font-semibold rounded-lg">
+                        Log In
+                    </Link>
+                </ul>   
+
+                <button className="md:hidden text-text-primary absolute top-2 right-2 transition-all duration-300" onClick={handleClick}>
+                    {!open ? (
+                        <MdMenu className="w-8 h-8" />
+                    ) : (
+                        <MdOutlineClose className="w-8 h-8" />
+                    )}
+                </button>           
+
+            </nav>
+            {open && (
+                <div className="menu-mobile bg-primary absolute top-0 bottom-0 left-0 z-50 w-60 transition-all translate-x-0 ease-in-out duration-500">
+                    <ul className="flex flex-col items-center justify-center space-y-6 h-full text-white">
+                        {navItems.map(( navItem ) => (
+                            <ActiveLink key={navItem.path} {...navItem} />
+                        ))}    
+                        <Link href="/login" className="flex items-center py-2 px-4 transition-all bg-gold-pastel text-primary font-heading font-semibold rounded-lg">
+                            Log In
+                        </Link>
+                    </ul>
+                </div>
+            )}
+        </>
     )
 }

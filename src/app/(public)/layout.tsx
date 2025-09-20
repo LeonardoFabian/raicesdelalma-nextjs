27b155798f9c1dfcd032f75fc4202aaf7dad5b3e
Leonardo Fabian <ramonlfabian@gmail.com>
2025-09-19 22:32:04 -0400

    Add condition to not getWishlistByUser if not session.user in PublicLayout

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Navbar, Footer, SideMenu, HydrateZustandWishlist } from "@/components";
 import { ScrollToTopButton } from "@/components";
 import { getWishlistByUser } from "@/actions";
+import { auth } from "@/auth.config";
 
 export const metadata: Metadata = {
   title: {
@@ -17,12 +18,18 @@ export default async function PublicLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const { products } = await getWishlistByUser({ page: 1, take: 100 });
-  const productIds = products.map((product) => product.id);
+  const session = await auth();
+  const user = session?.user;
+  let productIds: string[] = [];
+
+  if (user) {
+    const { products } = await getWishlistByUser({ page: 1, take: 100 });
+    productIds = products.map((product) => product.id);
+  }
 
   return (
     <>
-      {productIds && productIds.length > 0 && (
+      {user && productIds && productIds.length > 0 && (
         <HydrateZustandWishlist initialState={productIds} />
       )}
       <Navbar />
